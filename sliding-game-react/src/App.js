31750b6aec0ca48f8519659b3a469fb48102560a
Luka Licheli <lukalicheli@gmail.com>
2023-03-13 19:18:22 -0700

    Made the board where everytime i press start game on refresh it gives me a new board with random numbers ranging from 1-9

--- sliding-game-react/src/App.js
+++ sliding-game-react/src/App.js
@@ -1,14 +1,36 @@
 import "./App.css";
 import React, { useState } from "react";
 
-const winningBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const newBoard = [];
+const tilesNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i = 0; i < 9; i++) {
+  let randomIndex = Math.floor(Math.random() * tilesNumbers.length);
+  let value = tilesNumbers[randomIndex];
+  tilesNumbers.splice(randomIndex, 1);
+  newBoard.push(value);
+}
 
 function App() {
-  const [board, setBoard] = useState([]);
+  const [board, setBoard] = useState(Array(9).fill(""));
+
+  const startGame = (e) => {
+    setBoard(newBoard);
+  };
 
   return (
     <div className="App">
       <h1>Sliding Game</h1>
+      <button onClick={startGame}>Start Game</button>
+      <div className="board">
+        {board.map((tile, index) => {
+          return (
+            <div key={index} className="square">
+              {tile}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
